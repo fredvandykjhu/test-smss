3680a51cfb834da85875e4c45610910161203016
Fred Van Dyk <66218861+fredvandykjhu@users.noreply.github.com>
2023-11-14 22:26:04 -0500

    Update conf.py
    rename project , copyright and author

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'SMSS'
+copyright = '2023, JHBSPH'
+author = 'JHBSPH'
 
 release = '0.1'
 version = '0.1.0'
